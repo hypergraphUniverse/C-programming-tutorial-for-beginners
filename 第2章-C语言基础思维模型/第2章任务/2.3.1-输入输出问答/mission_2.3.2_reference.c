f289cf3c70bf222529adf06d6f0e46960f3f5f26
hypergraphUniverse <44737387+hypergraphUniverse@users.noreply.github.com>
2020-09-11 00:01:01 +0200

    correct the mistake in mission

--- 第2章-C语言基础思维模型/第2章任务/2.3.1-输入输出问答/mission_2.3.2_reference.c
+++ 第2章-C语言基础思维模型/第2章任务/2.3.1-输入输出问答/mission_2.3.2_reference.c
@@ -4,8 +4,8 @@ int main(void){
 	system("cls");
 	int income;
 	printf("Please input your monthly income:");
-	printf("____________$\b\b\b\b\b\b\b\b\b\b");
+	printf("$____________\b\b\b\b\b\b\b\b\b\b");
 	scanf("%d",&income);
-	printf("That\'s %d$ a year!\n",income*12);
+	printf("That\'s $%d a year!\n",income*12);
 	return 0; 
 } 
